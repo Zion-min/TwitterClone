0cd3fbece55854ecd0fd70c374164995c19c37c4
Zion-min <sioniasak@naver.com>
2023-05-16 17:39:08 +0900

    [feat] firebase 연동 코드, 패키지, 설정파일 추가

---
+++ TwitterClone/Core/Components/RoundedShape.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedShape.swift
+//  TwitterClone
+//
+//  Created by 민시온 on 2023/05/10.
+//
+
+import SwiftUI
+
+struct RoundedShape: Shape {
+    var corners: UIRectCorner
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: 80, height: 80))
+        return Path(path.cgPath)
+    }
+    
+}
